b97bb15790e1455732d5bec32fff94a51c412ead
Aurelia Robu <robuaurelia@gmail.com>
2023-08-19 15:03:52 +0300

    Contact details

--- src/components/CustomerReagistrationForm.tsx
+++ src/components/CustomerReagistrationForm.tsx
@@ -1,35 +1,64 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 interface CustomerRegistrationFormProps {
   onSubmit: (data: CustomerFormData) => void;
 }
 
 interface CustomerFormData {
+  title: string;
   firstName: string;
   middleName: string;
   lastName: string;
+  phoneNumber: string;
+  emailAddress: string;
+  preferredContact: 'phone' | 'email';
 }
 
-const initialCustomerFormData: CustomerFormData = {
+type Action =
+  | { type: 'SET_TITLE'; payload: string }
+  | { type: 'SET_FIRST_NAME'; payload: string }
+  | { type: 'SET_MIDDLE_NAME'; payload: string }
+  | { type: 'SET_LAST_NAME'; payload: string }
+  | { type: 'SET_PHONE_NUMBER'; payload: string }
+  | { type: 'SET_EMAIL_ADDRESS'; payload: string }
+  | { type: 'SET_PREFERRED_CONTACT'; payload: 'phone' | 'email' };
+
+const initialState: CustomerFormData = {
+  title: '',
   firstName: '',
   middleName: '',
   lastName: '',
+  phoneNumber: '',
+  emailAddress: '',
+  preferredContact: 'email',
+};
+
+const formReducer = (
+  state: CustomerFormData,
+  action: Action
+): CustomerFormData => {
+  switch (action.type) {
+    case 'SET_TITLE':
+      return { ...state, title: action.payload };
+    case 'SET_FIRST_NAME':
+      return { ...state, firstName: action.payload };
+    case 'SET_MIDDLE_NAME':
+      return { ...state, middleName: action.payload };
+    case 'SET_LAST_NAME':
+      return { ...state, lastName: action.payload };
+    case 'SET_PHONE_NUMBER':
+      return { ...state, phoneNumber: action.payload };
+    case 'SET_EMAIL_ADDRESS':
+      return { ...state, emailAddress: action.payload };
+    case 'SET_PREFERRED_CONTACT':
+      return { ...state, preferredContact: action.payload };
+    default:
+      return state;
+  }
 };
 
 function CustomerRegistrationForm({ onSubmit }: CustomerRegistrationFormProps) {
-  const [formData, setFormData] = useState<CustomerFormData>(
-    initialCustomerFormData
-  );
-
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    field: keyof CustomerFormData
-  ) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      [field]: e.target.value,
-    }));
-  };
+  const [formData, dispatch] = useReducer(formReducer, initialState);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -38,30 +67,100 @@ function CustomerRegistrationForm({ onSubmit }: CustomerRegistrationFormProps) {
 
   return (
     <div className="p-4">
-      <h2 className="text-lg font-semibold mb-4">Personal Information</h2>
+      <h1 className="text-2xl font-bold mb-4">Customer Registration Form</h1>
       <form onSubmit={handleSubmit} className="space-y-4">
-        <div className="flex space-x-4">
+        <h2 className="text-lg font-semibold mb-4">Personal Information</h2>
+        <div className="flex flex-col space-y-4">
+          <select
+            value={formData.title}
+            onChange={(e) =>
+              dispatch({ type: 'SET_TITLE', payload: e.target.value })
+            }
+            className="border rounded p-2 w-1/6"
+          >
+            <option value="" disabled>
+              Select Title
+            </option>
+            <option value="ms">Ms</option>
+            <option value="mr">Mr</option>
+            <option value="miss">Miss</option>
+            <option value="mrs">Mrs</option>
+            <option value="dr">Dr</option>
+          </select>
           <input
             type="text"
             placeholder="First Name"
             value={formData.firstName}
-            onChange={(e) => handleChange(e, 'firstName')}
-            className="border rounded p-2 w-1/3"
+            onChange={(e) =>
+              dispatch({ type: 'SET_FIRST_NAME', payload: e.target.value })
+            }
+            className="border rounded p-2 w-1/4"
           />
           <input
             type="text"
             placeholder="Middle Name"
             value={formData.middleName}
-            onChange={(e) => handleChange(e, 'middleName')}
-            className="border rounded p-2 w-1/3"
+            onChange={(e) =>
+              dispatch({ type: 'SET_MIDDLE_NAME', payload: e.target.value })
+            }
+            className="border rounded p-2 w-1/4"
           />
           <input
             type="text"
             placeholder="Last Name"
             value={formData.lastName}
-            onChange={(e) => handleChange(e, 'lastName')}
-            className="border rounded p-2 w-1/3"
+            onChange={(e) =>
+              dispatch({ type: 'SET_LAST_NAME', payload: e.target.value })
+            }
+            className="border rounded p-2 w-1/4"
           />
+          <h2 className="text-lg font-semibold mb-2">Contact details</h2>
+          <input
+            type="text"
+            placeholder="Phone Number"
+            value={formData.phoneNumber}
+            onChange={(e) =>
+              dispatch({ type: 'SET_PHONE_NUMBER', payload: e.target.value })
+            }
+            className="border rounded p-2 w-1/4"
+          />
+          <input
+            type="email"
+            placeholder="Email Address"
+            value={formData.emailAddress}
+            onChange={(e) =>
+              dispatch({ type: 'SET_EMAIL_ADDRESS', payload: e.target.value })
+            }
+            className="border rounded p-2 w-1/4"
+          />
+          <div className="col-span-2">
+            <label className="block mb-2" htmlFor="phone-radio-input">
+              <input
+                id="phone-radio-input"
+                type="radio"
+                value="phone"
+                checked={formData.preferredContact === 'phone'}
+                onChange={() =>
+                  dispatch({ type: 'SET_PREFERRED_CONTACT', payload: 'phone' })
+                }
+                className="mr-2"
+              />
+              Phone
+            </label>
+            <label className="block mb-2" htmlFor="email-radio-input">
+              <input
+                id="email-radio-input"
+                type="radio"
+                value="email"
+                checked={formData.preferredContact === 'email'}
+                onChange={() =>
+                  dispatch({ type: 'SET_PREFERRED_CONTACT', payload: 'email' })
+                }
+                className="mr-2"
+              />
+              Email
+            </label>
+          </div>
         </div>
         <button
           type="submit"
